dd93e4ebcb1033a6e8bd2a4fcfdf21d582611764
MridulChamoli <146312935+mridulchamoli93@users.noreply.github.com>
2025-12-04 12:39:59 +0530

    Delete Sorting-visualizations-main/BarGraph.py

--- Sorting-visualizations-main/BarGraph.py
+++
@@ -1,9 +0,0 @@
-def Bar_Graph(a,b,c):
-   import matplotlib.pyplot as plt
-   x=["Quick Sort","Insertion Sort","Bubble Sort"]
-   y=[a,b,c]
-   plt.bar(x, y, color='teal')
-   plt.xlabel("Sorting Algorithm")
-   plt.ylabel("Time Taken to Compute(In Seconds)")
-   plt.title("Comparsion Of Sorting Algorithm")
-   plt.show()
